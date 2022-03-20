19540b1b49f25429a4e34471f9782576a7dba87d
rhisth <96009471+rhisth@users.noreply.github.com>
2022-03-20 12:58:59 +0000

    Create getcats.py

---
+++ cmds/single/getcats.py
@@ -0,0 +1,17 @@
+import requests
+from os import chdir
+from time import sleep
+
+def cat(count, folder):
+    id = 0
+    chdir(folder)
+    for i in range(count):
+        id += 1
+        try:
+            sleep(1)
+            response = requests.get("https://thiscatdoesnotexist.com")
+            with open(f'cat{id}.png', 'wb') as file:
+                file.write(response.content)
+                file.close()
+        except:
+            print("Error.")
